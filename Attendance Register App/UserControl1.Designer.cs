64a3968d5363dd82be316152209f4182087d1864
Adhwaith K P <eahkfsgx@gmail.com>
2025-06-04 10:16:12 +0530

    User COntrol Addition

---
+++ Attendance Register App/UserControl1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Attendance_Register_App
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1title
+            // 
+            this.label1title.AutoSize = true;
+            this.label1title.Location = new System.Drawing.Point(3, 5);
+            this.label1title.Name = "label1title";
+            this.label1title.Size = new System.Drawing.Size(44, 16);
+            this.label1title.TabIndex = 0;
+            this.label1title.Text = "label1";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1title);
+            this.MinimumSize = new System.Drawing.Size(84, 53);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(191, 53);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1title;
+    }
+}
